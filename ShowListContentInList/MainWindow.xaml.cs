d545015cbccb0ea78f7237c53e9f4ab57f3c8036
tb2johm <tb2johm@gmail.com>
2014-09-03 14:27:32 +0200

    initial version

---
+++ ShowListContentInList/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Windows;
+
+namespace ShowListContentInList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class VM
+    {
+        public List<dynamic> ObjectList { get; set; }
+
+        public VM()
+        {
+            ObjectList = new List<dynamic>();
+
+            var ex = new ExpandoObject();
+            var dict = (IDictionary<string, object>) ex;
+            dict["test1"] = "markus";
+            dict["test2"] = "anders";
+            ObjectList.Add(ex);
+            
+            dict["test1"] = "arvid";
+            dict["test2"] = "erik";
+            ObjectList.Add(ex);
+
+
+        }
+    }
+}
